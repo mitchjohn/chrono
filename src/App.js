6732d1b5e7b18e4d0b0ffe8cee86afcbdd5809d5
mitch_roberge@hotmail.com <git config --global user.email votreemail@example.com>
2023-11-19 15:07:59 -0500

    Description des modifications

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Chronometre from './suite/chrono.js';
 import "./suite/chrono.css"
 import "./suite/ym.js"
 import Playlist from './suite/ym.js';
-import ImproviseThemeGenerator from './suite/generateur.js';
+//import ImproviseThemeGenerator from './suite/generateur.js';
 
 function App() {
   return (
